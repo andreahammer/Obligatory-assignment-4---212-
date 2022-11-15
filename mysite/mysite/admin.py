580c97dfaaab9311b23ab40742027ed0840744dc
Andrea.Ellefsrud <wux015@uib.no>
2022-11-15 18:40:25 +0100

    <commit_message>

--- mysite/mysite/admin.py
+++ mysite/mysite/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Car
 from .models import Customer
+
 from .models import Employee
+from .customer import Customer
+from .employee import Employee
+
 
 
 admin.site.register(Car)
